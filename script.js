2217f666b7ca4c88775c7e82647a7e7c9fdaee80
julio serafim <juliodev26@gmail.com>
2024-08-08 21:25:42 +0200

    estilizando main

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 
     const rulesImg = document.querySelector(".rulesImg");
 
-    document.getElementById("rules").addEventListener("click",(e)=> rulesImg.style.textCss);
-
-    document.getElementById("rules-close").addEventListener("click",(e)=>{rulesImg.style.display = "none"})
+    document.getElementById("rules").addEventListener("click",(e)=> rulesImg.style.cssText=`
+        visibility:visible;
+        width: 90%;
+        height: 90%;
+        top: 5%;
+        `);
+    document.getElementById("rules-close").addEventListener("click",(e)=>{rulesImg.style.cssText=`
+        visibility:hidden;
+        width: 0%;
+        height: 0%;
+        top: -100%;
+    `})
